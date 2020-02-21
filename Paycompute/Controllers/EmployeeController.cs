b429a38d5e5259fb044166a4fdc9ee061bdee5f2
Mark A. Rudesyle <mrudesyle@kpkeng.local>
2020-02-21 15:58:57 -0700

    Working through the tutorial

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Services;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
