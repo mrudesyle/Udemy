333065c88e1bc7ba8ea9b35d4e1973c49dcda3e6
Mark A. Rudesyle <mrudesyle@kpkeng.local>
2020-02-25 16:37:04 -0700

    Progressing through the tutorial

---
+++ Paycompute/Models/EmployeeCreateViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Models
+{
+    public class EmployeeCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string EmployeeNo { get; set; }
+
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string MiddleName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+
+        [Required]
+        public string NationalInsuranceNumber { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public StudentLoan StudentLoan { get; set; }
+        public UnionMember UnionMember { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        [Required]
+        public string Postcode { get; set; }
+    }
+}
