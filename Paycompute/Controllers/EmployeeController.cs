7e6136aaaed588015ec2484002a6bc0533b0eea0
mrudesyle <mrudesyle@outlook.com>
2020-09-28 14:53:13 -0600

    Layout, Index-EmployeeController

--- Paycompute/Controllers/EmployeeController.cs
+++ Paycompute/Controllers/EmployeeController.cs
@@ -15,6 +15,8 @@ namespace Paycompute.Controllers
     {
         private readonly IEmployeeService _employeeService;
         private readonly HostingEnvironment _hostingEnvironment;
+
+        //inject IEmployee service
         public EmployeeController(IEmployeeService employeeService, HostingEnvironment hostingEnvironment)
         {
             _employeeService = employeeService;
@@ -168,5 +170,61 @@ namespace Paycompute.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Detail(int id) 
+        {
+            //render employee details to the view
+            var employee = _employeeService.GetById(id);
+            if (employee == null) 
+            {
+                return NotFound();
+            }
+            EmployeeDetailViewModel model = new EmployeeDetailViewModel()
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                DOB = employee.DOB,
+                DateJoined = employee.DateJoined,
+                Designation = employee.Designation,
+                NationalInsuranceNumber = employee.NationalInsuranceNumber,
+                Phone = employee.Phone,
+                Email = employee.Email,
+                PaymentMethod = employee.PaymentMethod,
+                StudentLoan = employee.StudentLoan,
+                UnionMember = employee.UnionMember,
+                Address = employee.Address,
+                City = employee.City,
+                ImageUrl = employee.ImageUrl,
+                Postcode = employee.Postcode
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            var model = new EmployeeDeleteViewModel()
+            {
+                Id = employee.Id,
+                FullName = employee.FullName
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(EmployeeDeleteViewModel model)
+        {
+            await _employeeService.Delete(model.Id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
